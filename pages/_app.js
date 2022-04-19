fad9bc36a5a9e6d55966eb479b625e2fe3bb1334
tonyho <wai921114@gmail.com>
2022-04-19 18:37:26 +0800

    search page, search bar update

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,17 @@
 import '../styles/globals.css'
+import ProgressBar from "@badrap/bar-of-progress";
+import  Router from 'next/router';
+
+const progress = new ProgressBar({
+  size:4,
+  color:"#FE595E",
+  className:'z-50',
+  delay:100,
+});
+
+Router.events.on('routerChangeStart', progress.start);
+Router.events.on('routerChangeComplete', progress.finish);
+Router.events.on('routerChangeError', progress.finish);
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
